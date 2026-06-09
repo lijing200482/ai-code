1811bedcfc69f59cacd5111f04258a9c872d2bb1
zijing <3208132191@qq.com>
2026-06-09 13:12:10 +0800

    用户管理

---
+++ src/main/java/com/zijing/aicode/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.zijing.aicode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.zijing.aicode.entity.po.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author zijing
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/main/java/com/zijing/aicode/service/UserService.java
@@ -0,0 +1,80 @@
+package com.zijing.aicode.service;
+
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.zijing.aicode.entity.dto.UserQueryRequest;
+import com.zijing.aicode.entity.dto.UserVO;
+import com.zijing.aicode.entity.po.User;
+import com.zijing.aicode.entity.vo.LoginUserVO;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+
+/**
+ * 用户 服务层。
+ *
+ * @author zijing
+ */
+public interface UserService extends IService<User> {
+
+
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return
+     */
+    boolean userLogout(HttpServletRequest request);
+
+
+    /**
+     * 获取用户查询条件
+     *
+     * @param userQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(UserQueryRequest userQueryRequest);
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    UserVO getUserVO(User user);
+
+    List<UserVO> getUserVOList(List<User> userList);
+
+    String getEncryptPassword(String userPassword);
+}
